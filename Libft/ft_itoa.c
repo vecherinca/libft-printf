ea0bf83fd4bb98717f52c70916f2eabc43036c01
Mariia KLIMINA <mklimina@e2r9p20.clusters.42paris.fr>
2022-11-11 01:08:27 +0100

    itoa not working goin to sleep

---
+++ Libft/ft_itoa.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mklimina <mklimina@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/11 00:41:18 by mklimina          #+#    #+#             */
+/*   Updated: 2022/11/11 01:05:59 by mklimina         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+// this cute little function counts number of numbers in int
+int count_nmb(int n)
+{
+	int i;
+
+	i = 1;
+	while (n / 10)
+	{	
+		i++;
+		n = n/10;
+	}
+	return (i);
+		
+}
+char *ft_itoa(int n)
+{
+	char *output;
+	int i;
+	int len;
+
+	i = 0;
+	len = count_nmb(n) + 1; // +1 for a '\0'
+	output = malloc(len * sizeof(char));
+	if (!output)
+		return(NULL);
+	output[len - 1] = '\0';
+	if (n > 0)
+	{
+		output[i] = '0' + n/10;
+		
+	}
+	
+}
+int main()
+{
+	
+}
