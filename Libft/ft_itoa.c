3c4483845a8ea0e5ef23f75eb4f97fe4d1b6d93c
Mariia KLIMINA <mklimina@e2r12p3.clusters.42paris.fr>
2022-11-11 22:30:14 +0100

    itoa is working but I think there is a norm issue

--- Libft/ft_itoa.c
+++ Libft/ft_itoa.c
@@ -6,42 +6,70 @@
 /*   By: mklimina <mklimina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 00:41:18 by mklimina          #+#    #+#             */
-/*   Updated: 2022/11/11 18:22:56 by mklimina         ###   ########.fr       */
+/*   Updated: 2022/11/11 22:27:30 by mklimina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
+#include <unistd.h>
 // this cute little function counts number of numbers in int
 int count_nmb(int n)
 {
 	int i;
 
 	i = 1;
+	if (n < 0)
+	{
+		i = 2;
+		n = -n;
+	}
 	while (n / 10)
-	{	
+	{
 		i++;
-		n = n/10;
+		n = n / 10;
 	}
 	return (i);
-		
 }
 char *ft_itoa(int n)
 {
 	char *output;
 	int i;
 	int len;
+	long int num;
+	num = (long)n;
 
-	i = 0;
-	len = count_nmb(n) + 1; // +1 for a '\0'
-	output = malloc(len * sizeof(char));
+	len = count_nmb(num); // +1 for a '\0'
+	i = len - 1;
+	output = malloc(len * sizeof(char) + 1);
 	if (!output)
-		return(NULL);
-	output[len - 1] = '\0';
-	if (n > 0)
+		return (NULL);
+	output[len] = '\0';
+	if (num == 0)
 	{
-		output[i] = '0' + n/10;
-		
+		output[0] = '0';
 	}
-	
+	if (num < 0)
+	{
+		output[0] = 45;
+		num = -1 * num;
+	}
+	if (num > 0)
+	{
+		while (num > 0)
+		{
+			output[i] = '0' + num % 10;
+			num = num / 10;
+			i--;
+		}
+	}
+	return (output);
 }
+
+// int main ()
+// {
+//     int i;
+//     i = 0;
+
+//     printf("what is inside malloc ----> %s\n", ft_itoa(i));
+// }
