213deaae556bb73b86b6321abf3dde3b801e4958
Mariia KLIMINA <mklimina@e2r12p3.clusters.42paris.fr>
2022-11-11 23:26:54 +0100

    strjoin works

--- Libft/ft_strjoin.c
+++ Libft/ft_strjoin.c
@@ -6,54 +6,43 @@
 /*   By: mklimina <mklimina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 00:47:56 by mklimina          #+#    #+#             */
-/*   Updated: 2022/11/11 19:49:18 by mklimina         ###   ########.fr       */
+/*   Updated: 2022/11/11 23:26:06 by mklimina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
-
-char	*ft_strcpy(char *dest, char *src)
-{
-	int	i;
-
-	i = 0;
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	return (dest);
-}
-
-int	len_str(int size, char **strs)
-{
-	int	i;
-	int	len;
-
-	i = 0;
-	len = 0;
-	while (i < size)
-	{
-		len += ft_strlen(strs[i]);
-		i++;
-	}
-	return (len);
-}
 
 char *ft_strjoin(char const *s1, char const *s2)
 {
-	int		i;
-	int		j;
-	int		len;
-	char	*output;
-	
+    int i;
+    int j;
+    int len;
+    char *output;
+
+    i = 0;
+    j = 0;
+    len = ft_strlen(s1) + ft_strlen(s2) + 1;
+    output = malloc(sizeof(char) * len);
+    output[len - 1] = '\0';
+    if (!output)
+        return (NULL);
+    output[0] = 0;
+    while (s1[i] != '\0')
+    {
+        output[i] = s1[i];
+        i++;
+    }
+    while (s2[j] != '\0')
+    {
+        output[i] = s2[j];
+        i++;
+        j++;
+    }
+	return(output);
 }
+// int main()
+// {
+//     char s1[] = "I love Turells chips";
+//     char s2[] = "very much";
+//     printf("result ----> %s\n", ft_strjoin(s1, s2));
+// }
