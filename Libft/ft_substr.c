61d4490fbbac4d62e95e3e6f852129392257e476
Mariia KLIMINA <mklimina@e1r5p18.clusters.42paris.fr>
2022-11-14 11:53:04 +0100

    it's working

--- Libft/ft_substr.c
+++ Libft/ft_substr.c
@@ -6,34 +6,40 @@
 /*   By: mklimina <mklimina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/13 17:50:41 by mklimina          #+#    #+#             */
-/*   Updated: 2022/11/13 21:00:31 by mklimina         ###   ########.fr       */
+/*   Updated: 2022/11/14 11:51:04 by mklimina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
+
 char *ft_substr(char const *s, unsigned int start, size_t len)
 {
-	unsigned int i;
-	unsigned int l;
-	char *output;
-	l = ft_strlen(s);
-	if(len > l)
-		return(NULL);
-	output = (char *)malloc(len+1*sizeof(char));
-	if (!output)
-		return(NULL);
-	i = 0;
-	ft_bzero(output, len);
-	while(len > 0)
-	{
-		output[i] = s[start];
-		i++;
-		start++;
-		len--;
-	}
-	return(output);
+        char *output;
+		int lenstr;
+		lenstr = ft_strlen(s);
+        size_t j;
+
+        j = 0;
+		if (!s)
+			return (0);
+		if (start > lenstr)
+			return(ft_strdup(""));
+		if (len >= lenstr)
+			output = (char*)ft_calloc(lenstr - start + 1,sizeof(char));
+		else
+        	output = (char*)ft_calloc(len + 1, sizeof(char));
+        if (!output)
+            return(NULL);
+        while (j < len && s[start]!='\0')
+        {
+            output[j] = s[start]; 
+            start++;
+            j++;      
+        }
+        return(output);
 }
+
 // int main()
 // {
 //   char s[] = "tripouille";
