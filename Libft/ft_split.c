2e7156c2e01111e821cceb0dc3b0bc2aea373485
Mariia KLIMINA <mklimina@e2r4p2.clusters.42paris.fr>
2022-11-15 20:02:33 +0100

    SPLIT IS WORKING / to fix all the test cause a lot of errors but the structure est la

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: mklimina <mklimina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/13 19:55:31 by mklimina          #+#    #+#             */
-/*   Updated: 2022/11/15 19:34:00 by mklimina         ###   ########.fr       */
+/*   Updated: 2022/11/15 19:59:25 by mklimina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,9 +50,8 @@ void create_tab(char **tab, char const *s, char c)
     i = 0;
     index = 0;
     len = 0;
-
-  // this thing is now working yay
-  // first while we changed to (i < ft_strlen) hehe
+    // this thing is now working yay
+    // first while we changed to (i < ft_strlen) hehe
     while (i < ft_strlen(s))
     {
         while (!ischr(s[i], c) && s[i])
@@ -61,7 +60,7 @@ void create_tab(char **tab, char const *s, char c)
             len++;
         }
         tab[index] = malloc(sizeof(char) * len + 1);
-		tab[index][len] = '\0';
+        tab[index][len] = '\0';
         i++;
         index++;
         len = 0;
@@ -70,7 +69,26 @@ void create_tab(char **tab, char const *s, char c)
 
 void fill_tab(char **tab, char const *s, char c)
 {
-	
+    int i;
+    int j;
+    int index;
+    
+    i = 0;
+    index = 0;
+    j = 0;
+    // probably will be chanhed as in function create tab
+    while (i < ft_strlen(s)) // while we are not in the end of a string
+    {
+        while (!ischr(s[i], c) && s[i])
+        {
+            tab[index][j] = s[i];
+            j++;
+            i++;
+        }
+        i++;
+        index++;
+        j = 0;
+    }
 }
 
 char **ft_split(char const *s, char c)
@@ -80,14 +98,18 @@ char **ft_split(char const *s, char c)
     // first malloc is done, now I need to do the len of word and 2-nd malloc it
     number_words = numb_words(s, c);
     tab = malloc(sizeof(char *) * (number_words + 1));
+	if (!tab)
+		return(0);
     tab[number_words] = 0;
     create_tab(tab, s, c);
+    fill_tab(tab, s, c);
+	return(tab);
 }
 
-int main()
-{
-    char test[] = "I-love-Turells-chips";
-    char c;
-    c = 45;
-    ft_split(test, c);
-}
+// int main()
+// {
+//     char test[] = "I-love-Turells-chips";
+//     char c;
+//     c = 45;
+//     ft_split(test, c);
+// }
