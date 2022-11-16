3a7e5f338f8e2f036da0460c43fac9c6b8d633e1
Mariia KLIMINA <mklimina@e3r4p5.clusters.42paris.fr>
2022-11-16 20:04:36 +0100

    split is working/ fix the return shit please

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: mklimina <mklimina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/13 19:55:31 by mklimina          #+#    #+#             */
-/*   Updated: 2022/11/15 22:11:18 by mklimina         ###   ########.fr       */
+/*   Updated: 2022/11/16 19:56:35 by mklimina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 /*
 checking if char is separator or not
 */
+// don't forget to change strlen ok?
 int ischr(char c, char cc)
 {
     if (c == cc)
@@ -22,9 +23,10 @@ int ischr(char c, char cc)
     else
         return (0);
 }
-// this function needs to be fixed lol 
-//it fails when the string starts with the sep
-// to fix it 
+
+// this function needs to be fixed lol
+// it fails when the string starts with the sep
+// to fix it
 int numb_words(char const *s, char c)
 {
     int i;
@@ -35,14 +37,14 @@ int numb_words(char const *s, char c)
     {
         while (ischr(s[i], c))
             i++;
-        if (!ischr(s[i], c))
+        if (!ischr(s[i], c) && s[i] != '\0')
             count++;
         while (!ischr(s[i], c) && s[i])
             i++;
     }
     return (count);
 }
-// fails when more than two cons seps and when the string starts with sep too 
+// fails when more than two cons seps and when the string starts with sep too
 void create_tab(char **tab, char const *s, char c)
 {
     int i;
@@ -52,18 +54,18 @@ void create_tab(char **tab, char const *s, char c)
     i = 0;
     index = 0;
     len = 0;
-    // this thing is now working yay
-    // first while we changed to (i < ft_strlen) hehe
-    while (i < ft_strlen(s))
+    while (i < ft_strlen(s) - 1)
     {
-        while (!ischr(s[i], c) && s[i])
-        {
+        while (ischr(s[i], c) && s[i] && s[i] != '\0')
             i++;
+        while (!(ischr(s[i], c)) && s[i])
+        {
             len++;
+            i++;
         }
         tab[index] = malloc(sizeof(char) * len + 1);
-		if (!tab[index])
-			return(NULL);
+        if (!tab[index])
+            return (0);
         tab[index][len] = '\0';
         i++;
         index++;
@@ -76,44 +78,46 @@ void fill_tab(char **tab, char const *s, char c)
     int i;
     int j;
     int index;
-    
+
     i = 0;
     index = 0;
     j = 0;
-    // probably will be chanhed as in function create tab
-    while (i < ft_strlen(s)) // while we are not in the end of a string
+    while (i < ft_strlen(s) - 1)
     {
-        while (!ischr(s[i], c) && s[i])
+        while (ischr(s[i], c) && s[i] && s[i] != '\0')
+            i++;
+        while (!(ischr(s[i], c)) && s[i])
         {
             tab[index][j] = s[i];
             j++;
             i++;
         }
-        i++;
         index++;
+        i++;
         j = 0;
     }
 }
-
 char **ft_split(char const *s, char c)
 {
     char **tab;
     int number_words;
     // first malloc is done, now I need to do the len of word and 2-nd malloc it
     number_words = numb_words(s, c);
+    if (number_words == 0)
+        return(ft_calloc(1, sizeof(char *)));
     tab = malloc(sizeof(char *) * (number_words + 1));
-	if (!tab)
-		return(0);
+    if (!tab)
+        return (0);
     tab[number_words] = 0;
     create_tab(tab, s, c);
     fill_tab(tab, s, c);
-	return(tab);
+    return (tab);
 }
 
 // int main()
 // {
-//     char test[] = "I-love-Turells-chips";
-//     char c;
-//     c = 45;
-//     ft_split(test, c);
+//     // char test[] = "----I-love-Turells-chips";
+//     // char c;
+//     // c = 45;
+//     ft_split("  tripouille  42  ", ' ');
 // }
