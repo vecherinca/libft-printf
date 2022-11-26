080c80dd5274d0f9a5a479a1285c0c34af2e1a15
Mariia KLIMINA <mklimina@e2r12p7.clusters.42paris.fr>
2022-11-27 00:35:16 +0100

    it's shit and it's not working

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -3,39 +3,48 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mklimina <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: mklimina <mklimina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/26 21:04:01 by mklimina          #+#    #+#             */
-/*   Updated: 2022/11/26 21:04:06 by mklimina         ###   ########.fr       */
+/*   Updated: 2022/11/27 00:32:28 by mklimina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-# include <unistd.h>
+#include <unistd.h>
 #include <stdarg.h>
-int ft_printf(const char *string, ...) //we don't have a var here so how do I call it lol
+#include "ft_printf.h"
+// it's not working and I am tired 
+
+void pourcentage(char c, const char *string, ...)
 {
-	va_list args; //can we use it hehe??? 
+	va_list args;
+	int p;
 	va_start(args, string);
+	
+	if (c == 'c')
+	{
+		p = va_arg(args, int);
+		write(1, &p, 1);
+	}	
+	va_end(args);
+}
+int ft_printf(const char *string, ...)
+{
 	int i;
-	char c;
 	i = 0;
 	while (string[i])
-	{	
-		if (string[i] == '%' && string[i + 1] != '%')
-		{
-			c = va_arg(args, int);	
-			write(1, &c, 1);
-			i = i + 2;
-		}
-		write(1, &string[i], 1);
+	{
+		if (string[i] != '%')
+			write(1, &string[i], 1);
+		else
+			pourcentage(string[i + 1], string);
 		i++;
-		va_end(args);
 	}
-	return(0);
+	return (0);
 }
 int main()
 {
-	char c = 65;
-	ft_printf("Hello here is my char %c, %c", c, 66, 77);
+	// char c = 65;
+	ft_printf("Hello here is my char %c", 99);
 }
--- printf/ft_printf.h
+++ printf/ft_printf.h
@@ -5,5 +5,6 @@
 # include <unistd.h>
 # include <stdlib.h>
 #include <stdarg.h>
-int ft_printf(const char *string, ...);
+int		ft_printf(const char *string, ...);
+void	ft_putnbr_fd(int n, int fd);
 #endif
