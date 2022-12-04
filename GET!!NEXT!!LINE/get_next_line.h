e63257faef86ba53f76be260900d70adb2846ec4
Mariia KLIMINA <mklimina@e1r11p14.clusters.42paris.fr>
2022-12-04 17:57:06 +0100

    another empty file here

---
+++ GET!!NEXT!!LINE/get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mklimina <mklimina@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/04 17:50:09 by mklimina          #+#    #+#             */
+/*   Updated: 2022/12/04 17:55:52 by mklimina         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+
+char *get_next_line(int fd);
+
+#endif
