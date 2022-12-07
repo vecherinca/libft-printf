13f8a5bc5d315f832aa5a5453c471d73a9a7e390
Mariia KLIMINA <mklimina@e2r11p17.clusters.42paris.fr>
2022-12-07 19:39:52 +0100

    it's printing the first line c'est deja pas mal

--- GET!!NEXT!!LINE/get_next_line.c
+++ GET!!NEXT!!LINE/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mklimina <mklimina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 17:49:56 by mklimina          #+#    #+#             */
-/*   Updated: 2022/12/06 22:08:50 by mklimina         ###   ########.fr       */
+/*   Updated: 2022/12/07 19:30:44 by mklimina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,12 +39,15 @@ void *fill_the_stash(char *buffer, char *stash)
 {
 	// here we need to manage the stash one more time
 	char *line;
-	while(!check_the_line(stash))
-	{
-		line = ft_strjoin(stash, buffer);
-	}
-	printf("%s", line);
-	return(line);
+	if (!check_the_line(stash))
+		{
+			line = ft_strjoin(stash, buffer);
+			stash = line;
+			//printf("this is the updated stash ------> %s\n", stash);
+			return(stash);
+		}
+	// return(line);
+	return (stash);
 }
 
 char	*get_next_line(int fd)
@@ -52,33 +55,28 @@ char	*get_next_line(int fd)
 	int		bytes;
 	// int		i;
 	static char	buffer[BUFFER_SIZE];
-	char *stash;
+	char		*stash;
 	
 	stash = malloc(ft_strlen(buffer) * sizeof(char) + 1);
 	bytes = read(fd, buffer, BUFFER_SIZE);
 	stash[BUFFER_SIZE+1] = '\0';
-	printf("this is your buffer ----> %s\n", buffer);
+	//printf("this is your buffer ----> %s\n", buffer);
 	printf("*****************************\n");
-	// so what we do now is literally filling the first processing of the stash
-	// to fix it later
 	ft_strlcpy(stash, buffer, ft_strlen(buffer)+1);
-	printf("this is your stash ----> %s\n", stash);
+	// at this point stash et buffer are the same
+	//printf("this is your stash ----> %s\n", stash);
 	bytes = bytes;
-	//but here I don't get like if we launch the func in a loop 
-	//how in the world we enter the fucking loop
-	// let's try like this now
-	
+
 	while (!check_the_line(buffer))
 	{
 		//now the buffer is filled till the '\n'
 		bytes = read(fd, buffer, BUFFER_SIZE);
-		fill_the_stash(buffer, stash);	
+		stash = fill_the_stash(buffer, stash);	
 		//here u parse the thing to your stash
-		
-		
-	}
 	
-	return (NULL);
+	}
+	printf("this is the final stash ------> %s\n", stash);
+	return (stash);
 }
 
 
@@ -93,6 +91,7 @@ int	main(void)
 	// 	printf("file descriptor -- > %d\n Masha your file is ok\n", fd);
 	// get_next_line(fd);
 	get_next_line(fd);
+	//get_next_line(fd);
 	//get_next_line(fd);
 	return (0);
 }
