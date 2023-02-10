6d23650a81485db8e565ad75049badb45b5e1d58
Mariia KLIMINA <mklimina@e1r12p20.clusters.42paris.fr>
2023-02-10 18:52:59 +0100

    index ok

--- push_swap/indexes.c
+++ push_swap/indexes.c
@@ -6,7 +6,7 @@
 /*   By: mklimina <mklimina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 19:59:44 by mklimina          #+#    #+#             */
-/*   Updated: 2023/02/10 16:56:32 by mklimina         ###   ########.fr       */
+/*   Updated: 2023/02/10 18:51:46 by mklimina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,31 @@ int find_min_next(a_list *start, int min)
 {
 	int new_min;
 	
-	new_min = start -> content;
+	//new_min = start -> content;
+	new_min = find_max(start);
 	while (start != NULL)
 	{
 		if ((start -> content < new_min) && (start -> content > min))
 			new_min = start -> content;
 		start = start -> next;
 	}
+	
 	return(new_min);
+}
+int find_max(a_list *start)
+{
+	a_list *max;
+	max = start;
+	while (start != NULL)
+	{
+		if (start -> content > max -> content)
+		{
+			//node = start;
+			max = start;
+		}
+		start = start -> next;
+	}
+	return(max -> content);
 }
 void	assign_index(head_a *a)
 {
@@ -41,17 +58,14 @@ void	assign_index(head_a *a)
 	
 	while (i < size)
 	{
-		while (start -> next!= NULL)
-		{
+		while (start != NULL)
+		{	
 			if (start -> content >= minmin)
-			{
 				start -> index = i;
-			}
 			start = start -> next;
 		}
-		// this shit is not working you need to check the last case
-		start -> index = i;
 		start = a -> first;
+		minmin = minmin;
 		minmin = find_min_next(start, minmin);
 		i++;
 	}
