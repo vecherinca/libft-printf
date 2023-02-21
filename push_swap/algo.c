4aacf1bb68da528a352894e67de4d1e69b7dd5e0
Masha Klimina <kliminaklimina@gmail.com>
2023-02-21 03:01:44 +0100

    algo started

--- push_swap/algo.c
+++ push_swap/algo.c
@@ -3,14 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   algo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mklimina <mklimina@student.42.fr>          +#+  +:+       +#+        */
+/*   By: maria <maria@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/12 15:10:56 by mklimina          #+#    #+#             */
-/*   Updated: 2023/02/12 17:43:56 by mklimina         ###   ########.fr       */
+/*   Updated: 2023/02/20 21:37:28 by maria            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "push_swap.h"
+#include "push_swap.h"
+
 
 // void assign_next_big(head_a *a, head_a *b)
 // {
--- push_swap/random100.c
+++ push_swap/random100.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   random100.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mklimina <mklimina@student.42.fr>          +#+  +:+       +#+        */
+/*   By: maria <maria@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 17:02:29 by mklimina          #+#    #+#             */
-/*   Updated: 2023/02/12 19:25:07 by mklimina         ###   ########.fr       */
+/*   Updated: 2023/02/21 03:01:07 by maria            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,24 +40,84 @@ void presort(int median, head_a *b, head_a *a)
 	}
 }
 
+void count_moves_a(a_list *node_a)
+{
+}
+
+a_list *get_nearest_max(a_list *b_node, head_a *a)
+{
+	int diff;
+	a_list *start_a;
+	a_list *first_max;
+	start_a = a->first;
+	diff = b_node -> index - start_a -> index;
+	first_max = start_a;
+	//printf("difference ----> %d", diff);
+	while (start_a)
+	{
+		if ((start_a -> index > b_node -> index) && (b_node -> index - start_a -> index > diff))
+			first_max = start_a;
+		start_a = start_a -> next;
+	}
+	return(first_max);
+}
+int number_moves(head_a *a,a_list *b_node, head_a *b, algo_values current)
+{
+	//count the moves for a/
+	//first you need to check the index lol but it's after
+	int count;
+	int size;
+	int total;
+	a_list *node;
+	a_list *node_a;
+	node = b -> first;
+	size = ft_lstsize(node);
+	//printf("size %d", size);
+	count = 0;
+	//printf("b_node -> content %d\n", b_node -> content);
+	//printf("node -> content %d\n", node -> content);
+	while (b_node != node)
+	{
+		count++;
+		node = node -> next;
+	}
+	if (count < size/2)
+		current.rb = count;
+	else if(count > size/2)
+		current.rrb = size - count + 1;
+	
+	//printf("current.rrb -- > %d\n", current.rrb);
+	//printf("current.rb -- > %d\n", current.rb);
+	node_a = get_nearest_max(b_node, a);
+	count_moves_a(node_a);
+	printf("max_node_a ---> %d\n", node_a -> content);
+	total = current.rrb + current.rb;
+	return(total);
+}
 void test_instr(head_a *a,head_a *b)
 {
 	// here what I want to do is for every node
 	// write the number of functions
 	a_list *start_a;
 	a_list *start_b;
+	int numb_moves;
+	algo_values current;
+	current.rra = 0;
+	current.ra = 0;
+	current.rrb = 0;
+	current.rb = 0;
 	
-	algo_values count_a;
-	algo_values count_b;
-
-	start_a = a -> first;
-	start_b = b -> first;
-	
-	while (start_b -> next != NULL)
-	{
-		
-		
-	}
+	numb_moves = 0;
+	current.node = b -> first;
+	start_b = a -> first;
+	//now you need to make a function that counts a number of 
+	//moves
+	//add here node a cause u r gonna send it to another function in order to calculate the sum
+	numb_moves = number_moves(a, current.node, b, current);
+	//printf("Number of moves --> %d\n", numb_moves);
+	//printf("current.node ----> %d\n", current.node -> content);
+	// also here you write a loop while 
+	// where you compare lol
 }
 
 void sort100(head_a *a,head_a *b)
@@ -66,7 +126,6 @@ void sort100(head_a *a,head_a *b)
 	a_list *start;
 	start = a -> first;
 	median = ft_lstsize(start)/2;
-	//printf("MEDIAN ----> %d\n", median);
 	presort(median, b, a);
 	test_instr(a, b);
 }
