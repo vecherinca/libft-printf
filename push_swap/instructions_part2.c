bd46acf05b36e2638791c039ae181e2f85323dc3
Masha Klimina <kliminaklimina@gmail.com>
2023-02-28 02:04:30 +0100

    for 500 it's ok for 100 sometimes shit

--- push_swap/instructions_part2.c
+++ push_swap/instructions_part2.c
@@ -6,7 +6,7 @@
 /*   By: maria <maria@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 22:52:53 by mklimina          #+#    #+#             */
-/*   Updated: 2023/02/27 03:24:14 by maria            ###   ########.fr       */
+/*   Updated: 2023/02/28 01:16:36 by maria            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,9 +57,12 @@ void rb(head_a *headb, int flag)
 // check the number of elements
 void rr(head_a *heada, head_a *headb)
 {
-	ra(heada, 0);
-	rb(headb, 0);
-	write(1, "rr\n", 3);
+	if (ft_lstsize(heada -> first)> 1 && ft_lstsize(headb -> first) > 1)
+	{
+		ra(heada, 0);
+		rb(headb, 0);
+		write(1, "rr\n", 3);
+	}
 }
 /**rra (reverse rotate a): Shift down all elements of stack a by 1.
 The last element becomes the first one.**/ 
@@ -105,7 +108,10 @@ void rrb(head_a *headb, int flag)
 
 void rrr(head_a *heada, head_a *headb)
 {
-	rra(heada, 0);
-	rrb(headb, 0);
-	write(1, "rrr\n", 4);
+	if (ft_lstsize(heada -> first)> 1 && ft_lstsize(headb -> first) > 1)
+	{
+		rra(heada, 0);
+		rrb(headb, 0);
+		write(1, "rrr\n", 4);
+	}
 }
--- push_swap/movefastest.c
+++ push_swap/movefastest.c
@@ -6,24 +6,66 @@
 /*   By: maria <maria@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/12 15:10:56 by mklimina          #+#    #+#             */
-/*   Updated: 2023/02/27 03:22:48 by maria            ###   ########.fr       */
+/*   Updated: 2023/02/28 01:45:36 by maria            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+algo_values max_pul(algo_values current)
+{
+    current.rr = 0;
+    current.rrr = 0;
+
+    if (current.rb > current.ra && current.ra > 0)
+    {
+        current.rr = current.ra;
+        current.ra = 0;
+        current.rb = current.rb - current.rr;
+    }
+    if (current.ra > current.rb && current.rb > 0)
+    {
+        current.rr = current.rb;
+        current.rb = 0;
+        current.ra = current.ra - current.rr;
+    }
+    if (current.ra == current.rb && (current.ra > 0 && current.rb > 0))
+    {
+        current.rr = current.rb;
+        current.rb = 0;
+        current.ra = 0;
+    }
+    
+    // *********
+    if (current.rrb > current.rra && current.rra > 0)
+    {
+        current.rrr = current.rra;
+        current.rra = 0;
+        current.rrb = current.rrb - current.rrr;
+    }
+    if (current.rra > current.rrb && current.rrb > 0)
+    {
+        current.rrr = current.rrb;
+        current.rrb = 0;
+        current.rra = current.rra - current.rrr;
+    }
+    if (current.rra == current.rrb && (current.rra > 0 && current.rrb > 0))
+    {
+        current.rrr = current.rrb;
+        current.rrb = 0;
+        current.rra = 0;
+    }
+    return(current);
+}
 void movefastest(algo_values current, head_a *a, head_a *b)
 {
-        // if (current.ra == current.rb)
-        // {
-        //     int rr_val;
-        //     rr_val = current.ra;
-        //     while (rr_val != 0)
-        //     {
-        //         rr(a, b);
-        //         rr_val--;
-        //     }
-        // }
+
+        current = max_pul(current);
+        while (current.rr != 0)
+        {
+            rr(a, b);
+            current.rr--;
+        }
         while (current.ra != 0)
         {
             ra(a, 1);
@@ -34,6 +76,11 @@ void movefastest(algo_values current, head_a *a, head_a *b)
             rb(b, 1);
             current.rb--;
         }
+        while (current.rrr != 0)
+        {
+            rrr(a, b);
+            current.rrr--;
+        }
         while (current.rra != 0)
         {
             rra(a, 1);
@@ -43,5 +90,5 @@ void movefastest(algo_values current, head_a *a, head_a *b)
         {
             rrb(b, 1);
             current.rrb--;
-        }
+        }      
 }
