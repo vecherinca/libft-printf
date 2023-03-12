e2e53031e87e0af64df02c58e43036eab2a93dcd
masha - ubuntu <nemashaklimina@gmail.com>
2023-03-12 20:38:27 +0000

    heh

--- fdf/fdf.c
+++ fdf/fdf.c
@@ -12,18 +12,52 @@
 
 #include "fdf.h"
 
-int	main(int argc, char **argv)
+# define WINDOW_WIDTH 600
+# define WINDOW_HEIGHT 300
+
+#define MLX_ERROR 1
+
+int main(void)
 {
-	char	*name;
-	t_tab	**table;
-	int		cnt;
-	void	*mlx;
-	(void)argc;
-	name = argv[1];
-	cnt = count_lines(name);
-	table = create_ttable(name);
-	table = table;
-	print(table, cnt);
-	mlx = mlx_init();
-	mlx = mlx;
+	void	*mlx_ptr;
+	void	*win_ptr;
+
+	mlx_ptr = mlx_init();
+	if (mlx_ptr == NULL)
+		return (MLX_ERROR);
+	win_ptr = mlx_new_window(mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "My first window!");
+	if (win_ptr == NULL)
+	{
+		free(win_ptr);
+		return (MLX_ERROR);
+	}
+	while (1)
+		;
+	mlx_destroy_window(mlx_ptr, win_ptr);
+	mlx_destroy_display(mlx_ptr);
+	free(mlx_ptr);
 }
+
+// int	main(int argc, char **argv)
+// {
+// 	char	*name;
+// 	t_tab	**table;
+// 	//int		cnt;
+// 	//void	*mlx_win;
+// 	(void)argc;
+// 	name = argv[1];
+// 	//cnt = count_lines(name);
+// 	table = create_ttable(name);
+// 	table = table;
+// 	// print(table, cnt);
+
+// 	void	*mlx_ptr;
+// 	void	*win_ptr;
+
+// 	mlx_ptr = mlx_init();
+// 	win_ptr = mlx_new_window(mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "My first window!");
+// 	mlx_destroy_window(mlx_ptr, win_ptr);
+// 	mlx_destroy_display(mlx_ptr);
+// 	free(mlx_ptr);
+
+// }
